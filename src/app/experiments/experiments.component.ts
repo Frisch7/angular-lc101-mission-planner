5e1f2b524fbc189ea5c7eca2f3c39a338f90ea8d
Dominic Frisch <frisch7@gmail.com>
2020-07-29 15:05:06 -0500

    finished bonus missions

--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiments.component.css']
 })
 export class ExperimentsComponent implements OnInit {
-
+  experimentBeingEdited : string = null;
+  experiments : string[] =[
+    'Mars soil sample',
+    'Plant growth in habitat',
+    'Human bone density'
+  ];
   constructor() { }
 
   ngOnInit() {
   }
-
+  addExperiment(experiment: string){
+    if(!this.experiments.includes(experiment)){
+    this.experiments.push(experiment);
+    }
+  }
+  save(newExperiment : string, experiment : string){
+    let index = this.experiments.indexOf(experiment);
+    this.experiments.splice(index, 1, newExperiment);
+  }
+  remove(experiment: string){
+    let index = this.experiments.indexOf(experiment);
+    this.experiments.splice(index, 1);
+    this.experimentBeingEdited = null;
+  }
+  edit(experiment: string){
+    this.experimentBeingEdited = experiment;
+  }
 }
